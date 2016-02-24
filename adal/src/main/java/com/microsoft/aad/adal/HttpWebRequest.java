b0c15367763929ea8c50966d6a22926a6ca99857
Irina Makhalova <imakhal@microsoft.com>
2016-02-24 14:50:30 -0800

    reverted comment move

--- adal/src/main/java/com/microsoft/aad/adal/HttpWebRequest.java
+++ adal/src/main/java/com/microsoft/aad/adal/HttpWebRequest.java
@@ -138,7 +138,7 @@ class HttpWebRequest {
                 mConnection.setUseCaches(mUseCaches);
                 mConnection.setRequestMethod(mRequestMethod);
                 mConnection.setDoInput(true); // it will at least read status
-                // code. Default is true.
+                                              // code. Default is true.
                 setRequestBody();
 
                 byte[] responseBody = null;
