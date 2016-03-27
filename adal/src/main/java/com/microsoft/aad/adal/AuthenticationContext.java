9643aa5548bdf6eb0f25fe80bf2d84e1f702fdd8
Irina Makhalova <imakhal@microsoft.com>
2016-03-27 01:54:33 -0700

    If AT was obtained by MRRT then don't check regular RT to refresh token

--- adal/src/main/java/com/microsoft/aad/adal/AuthenticationContext.java
+++ adal/src/main/java/com/microsoft/aad/adal/AuthenticationContext.java
@@ -1394,7 +1394,8 @@ public class AuthenticationContext {
 
         // Trying to find refresh token first, if existed, will try to use the refresh token. 
         Logger.v(TAG, "Checking refresh tokens");
-        RefreshItem refreshItem = getRefreshToken(request);
+        boolean useMultiResourceRefreshToken = cachedItem == null ? true : cachedItem.getIsMultiResourceRefreshToken();
+        RefreshItem refreshItem = getRefreshToken(request, useMultiResourceRefreshToken);
         AuthenticationResult authResult = null;
         if (!promptUser(request.getPrompt()) && refreshItem != null
                 && !StringExtensions.IsNullOrBlank(refreshItem.mRefreshToken)) {
@@ -1554,10 +1555,9 @@ public class AuthenticationContext {
         
         String mTenantId;
 
-        public RefreshItem(String keyInCache, AuthenticationRequest request, TokenCacheItem item,
-                boolean multiResource) {
+        public RefreshItem(String keyInCache, AuthenticationRequest request, TokenCacheItem item) {
             mKey = keyInCache;
-            mMultiResource = multiResource;
+            mMultiResource = item.getIsMultiResourceRefreshToken();
 
             if (item != null) {
                 mRefreshToken = item.getRefreshToken();
@@ -1579,30 +1579,31 @@ public class AuthenticationContext {
         }
     }
 
-    private RefreshItem getRefreshToken(final AuthenticationRequest request) {
+    private RefreshItem getRefreshToken(final AuthenticationRequest request, boolean useMultiResourceRefreshToken) {
         final String methodName = ":getRefreshToken";
         RefreshItem refreshItem = null;
         if (mTokenCacheStore != null) {
-            boolean multiResource = false;
-            // target refreshToken for this resource first. CacheKey will
-            // include the resourceId in the cachekey
-            Logger.v(TAG, "Looking for regular refresh token");
             String userId = request.getUserId();
             if (StringExtensions.IsNullOrBlank(userId)) {
                 // acquireTokenSilent expects userid field from UserInfo
                 userId = request.getLoginHint();
             }
-            String keyUsed = CacheKey.createCacheKey(request, userId);
-            TokenCacheItem item = mTokenCacheStore.getItem(keyUsed);
-            if (item == null || StringExtensions.IsNullOrBlank(item.getRefreshToken())) {
-                // if not present, check multiResource item in cache. Cache key
+            String keyUsed;
+            TokenCacheItem item;
+            if (!useMultiResourceRefreshToken) {
+                // target refreshToken for this resource only. CacheKey will
+                // include the resourceId in the cachekey
+                Logger.v(TAG, "Looking for regular refresh token");
+                keyUsed = CacheKey.createCacheKey(request, userId);
+                item = mTokenCacheStore.getItem(keyUsed);
+            } else {
+                // check multiResource item in cache. Cache key
                 // will not include resourceId in the cache key string.
                 Logger.v(TAG, "Looking for Multi Resource Refresh token");
                 keyUsed = CacheKey.createMultiResourceRefreshTokenKey(request, userId);
                 item = mTokenCacheStore.getItem(keyUsed);
-                multiResource = true;
             }
-            
+
             // If still cannot find an item, try with family clientid
             // Disable this feature for ADFS authority
             if (item == null || StringExtensions.IsNullOrBlank(item.getRefreshToken())) {
@@ -1619,7 +1620,7 @@ public class AuthenticationContext {
 
                 Logger.v(TAG, "Refresh token is available and id:" + refreshTokenHash
                         + " Key used:" + keyUsed);
-                refreshItem = new RefreshItem(keyUsed, request, item, multiResource);
+                refreshItem = new RefreshItem(keyUsed, request, item);
             }
         }
 
